02d936ebb0a37ccdc81774275f1f6af5f234aaa3
cbotek <botekchristophe@gmail.com>
2021-07-27 09:12:26 -0400

    Add clinvar as delta table

--- datalake-spark3/src/test/scala/bio/ferlab/datalake/spark3/testutils/WithSparkSession.scala
+++ datalake-spark3/src/test/scala/bio/ferlab/datalake/spark3/testutils/WithSparkSession.scala
@@ -10,6 +10,8 @@ trait WithSparkSession {
 
   private val tmp = new File("tmp").getAbsolutePath
   implicit lazy val spark: SparkSession = SparkSession.builder()
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     .enableHiveSupport()
     .master("local")
     .getOrCreate()
