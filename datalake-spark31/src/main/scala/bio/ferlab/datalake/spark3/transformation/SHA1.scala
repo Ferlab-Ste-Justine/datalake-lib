6591b86e12dd337d19e6830c464b5c728cd992cb
cbotek <cbotek@ferlab.bio>
2022-01-21 12:47:34 -0500

    UNIC-134 fix SHA1 and SHA2

--- datalake-spark31/src/main/scala/bio/ferlab/datalake/spark3/transformation/SHA1.scala
+++ datalake-spark31/src/main/scala/bio/ferlab/datalake/spark3/transformation/SHA1.scala
@@ -2,11 +2,12 @@ package bio.ferlab.datalake.spark3.transformation
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
 
 case class SHA1(override val columns: String*) extends HashTransformation {
   override def transform: DataFrame => DataFrame = { df =>
     columns.foldLeft(df){ case (d, column) =>
-      d.withColumn(column, sha1(col(column)))
+      d.withColumn(column, sha1(col(column).cast(StringType)))
     }
   }
 }
