cd400df6fa7ce9f5126cad556b83ff20a44668e4
Laura Bégin <lbegin@ferlab.bio>
2023-01-24 16:46:36 -0500

    refactor: CLIN-1669 Add curried cast functions

--- datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/transformation/Cast.scala
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/transformation/Cast.scala
@@ -1,8 +1,8 @@
 package bio.ferlab.datalake.spark3.transformation
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Column, DataFrame}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.types.{DataType, DoubleType, FloatType, IntegerType, LongType}
 
 case class Cast(dataType: DataType, columns: String*) extends Transformation {
   override def transform: DataFrame => DataFrame = { df =>
@@ -10,5 +10,18 @@ case class Cast(dataType: DataType, columns: String*) extends Transformation {
       d.withColumn(column, col(column).cast(dataType))
     }
   }
+}
+
+object Cast {
+
+  /**
+   * Cast a column while preserving its original name
+   */
+  val cast: String => DataType => Column = c => t => col(c).cast(t).as(c)
+
+  val castLong: String => Column = cast(_)(LongType)
+  val castInt: String => Column = cast(_)(IntegerType)
+  val castFloat: String => Column = cast(_)(FloatType)
+  val castDouble: String => Column = cast(_)(DoubleType)
 }
 
