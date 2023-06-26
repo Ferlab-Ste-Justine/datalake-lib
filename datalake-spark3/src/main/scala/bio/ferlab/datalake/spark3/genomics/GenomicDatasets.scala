37c5bd81ba6325a7805f7723832aba093efa273e
jecos <jcostanza@ferlab.bio>
2023-06-26 09:30:23 -0400

    fix: Partitions for gene_suggestions

--- datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/genomics/GenomicDatasets.scala
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/genomics/GenomicDatasets.scala
@@ -12,6 +12,6 @@ case class GenomicDatasets(alias: String, tableDatabase: Option[String], viewDat
     DatasetConf("es_index_variant_centric", alias, "/es_index/variant_centric", PARQUET, OverWrite, partitionby = List("chromosome"), table = table("variant_centric"), view = view("variant_centric"), repartition = Some(RepartitionByColumns(columnNames = Seq("chromosome"), n = Some(100)))),
     DatasetConf("es_index_variant_suggestions", alias, "/es_index/variant_suggestions", PARQUET, OverWrite, partitionby = List("chromosome"), table = table("variant_suggestions"), view = view("variant_suggestions"), repartition = Some(RepartitionByColumns(columnNames = Seq("chromosome"), n = Some(100)))),
     DatasetConf("es_index_gene_centric", alias, "/es_index/gene_centric", PARQUET, OverWrite, partitionby = List("chromosome"), table = table("gene_centric"), view = view("gene_centric"), repartition = Some(RepartitionByColumns(columnNames = Seq("chromosome"), n = Some(100)))),
-    DatasetConf("es_index_gene_suggestions", alias, "/es_index/gene_suggestions", PARQUET, OverWrite, partitionby = List("chromosome"), table = table("gene_suggestions"), view = view("gene_suggestions"), repartition = Some(RepartitionByColumns(columnNames = Seq("chromosome"), n = Some(100))))
+    DatasetConf("es_index_gene_suggestions", alias, "/es_index/gene_suggestions", PARQUET, OverWrite, partitionby = List("chromosome"), table = table("gene_suggestions"), view = view("gene_suggestions"), repartition = Some(FixedRepartition(20)))
   )
 }
