1b295981af54675060e0737380241a98ddffe46b
jecos <jcostanza@ferlab.bio>
2023-07-04 15:54:08 -0400

    feat: BG-10 use sttp for ES client

---
+++ datalake-spark3/src/test/scala/bio/ferlab/datalake/spark3/elasticsearch/ElasticSearchClientSpec.scala
@@ -0,0 +1,9 @@
+package bio.ferlab.datalake.spark3.elasticsearch
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{BeforeAndAfterAll, GivenWhenThen}
+
+class ElasticSearchClientSpec extends AnyFlatSpec with GivenWhenThen with Matchers with BeforeAndAfterAll {
+
+}
