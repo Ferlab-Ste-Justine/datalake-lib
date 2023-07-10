262976069f664735c217fa542c72489af1d620bf
Joël Simoneau <jsimoneau@ferlab.bio>
2023-07-10 11:37:50 -0400

    feat: adding BA1 criteria

---
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/implicits/ACMGImplicits.scala
@@ -0,0 +1,50 @@
+package bio.ferlab.datalake.spark3.implicits
+
+import org.apache.spark.sql.{Column, DataFrame, RelationalGroupedDataset, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StructType}
+
+
+object ACMGImplicits {
+
+  implicit class ACMGOperations(df: DataFrame) {
+
+    /**
+     * BA1 - ACMG criteria
+     * Stand-alone evidence of benign impact.
+     * Allele frequency is >5% in a large-scale sequencing project (1000 Genomes project, TOPMed BRAVO, gnomAD)
+     *
+     * Input DataFrame must contain an `external_frequencies` column.
+     *
+     * @return A struct containing the study with the highest AF, the AF and the BA1 score :
+     *         StructType(
+     *         StructField(study, StringType, true),
+     *         StructField(max_af, DoubleType, true),
+     *         StructField(score, BooleanType, true)
+     *         )
+     */
+    def get_BA1: Column = {
+
+      require(df.columns.contains("external_frequencies"), "Column `external_frequencies` is required for BA1.")
+
+      val afCols = for (field <- df.select("external_frequencies.*").columns) yield s"external_frequencies.${field}.af"
+
+      val structs = afCols.map(
+        c => struct(col(c).as("v"), lit(c).as("k"))
+      )
+
+      val study = split(greatest(structs: _*).getItem("k"), "\\.")(1)
+      val max_af = greatest(structs: _*).getItem("v")
+
+      struct(
+        study.as("study"),
+        max_af.as("max_af"),
+        (max_af >= 0.05).as("score")
+      )
+
+    }
+
+
+  }
+
+}
---
+++ datalake-spark3/src/test/scala/bio/ferlab/datalake/spark3/implicits/ACMGImplicitsSpec.scala
@@ -0,0 +1,72 @@
+package bio.ferlab.datalake.spark3.implicits
+
+import bio.ferlab.datalake.spark3.implicits.ACMGImplicits._
+import bio.ferlab.datalake.spark3.testutils.WithSparkSession
+import org.apache.spark.sql.{Row, DataFrame}
+import org.apache.spark.sql.types._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+
+
+class ACMGImplicitsSpec extends AnyFlatSpec with WithSparkSession with Matchers {
+
+  import spark.implicits._
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  "get_BA1" should "throw IllegalArgumentException if `external_frequencies` column is absent" in {
+    val structureData = Seq(
+      Row(1), Row(2)
+    )
+
+    val structureSchema = new StructType()
+      .add("start", IntegerType, true)
+
+    val df = spark.createDataFrame(
+      spark.sparkContext.parallelize(structureData), structureSchema)
+
+    an[IllegalArgumentException] should be thrownBy df.get_BA1
+  }
+
+  it should "return the correct BA1 classification" in {
+
+    val querySchema = new StructType()
+      .add("start", IntegerType, true)
+      .add("external_frequencies", new StructType()
+        .add("thousand_genomes", new StructType()
+          .add("af", DoubleType, true)
+          .add("an", IntegerType, true))
+        .add("topmed_bravo", new StructType()
+          .add("af", DoubleType, true)
+          .add("an", IntegerType, true)))
+
+    val queryData = Seq(
+      Row(1, Row(Row(0.001, 2), Row(0.050, 50))),
+      Row(2, Row(Row(0.010, 12), Row(0.001, 3))),
+    )
+
+    val resultSchema = new StructType()
+      .add("BA1", new StructType()
+        .add("study", StringType, true)
+        .add("max_af", DoubleType, true)
+        .add("score", BooleanType, true))
+
+    val resultData = Seq(
+      Row(Row("topmed_bravo", 0.050, true)),
+      Row(Row("thousand_genomes", 0.010, false))
+    )
+
+    val queryDF = spark.createDataFrame(
+      spark.sparkContext.parallelize(queryData), querySchema)
+
+    val resultDF = spark.createDataFrame(
+      spark.sparkContext.parallelize(resultData), resultSchema)
+
+    val result = queryDF.withColumn("BA1", queryDF.get_BA1).select("BA1")
+
+    resultDF.collect().sameElements(result.collect()) shouldEqual true
+  }
+
+}
