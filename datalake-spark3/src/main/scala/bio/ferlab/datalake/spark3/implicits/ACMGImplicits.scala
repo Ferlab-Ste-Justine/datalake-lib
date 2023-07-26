a9169b21ded2feb14392c7a891a9ac12034bcd37
Joël Simoneau <jsimoneau@ferlab.bio>
2023-07-26 16:47:38 -0400

    feat: adding basic BS2 logic

--- datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/implicits/ACMGImplicits.scala
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/implicits/ACMGImplicits.scala
@@ -6,6 +6,14 @@ import org.apache.spark.sql.{Column, DataFrame}
 
 object ACMGImplicits {
 
+  /**
+   * inColArray
+   * Anonymous helper function generating boolean columns for array-containing columns.
+   *
+   * @return a Column of boolean
+   */
+  val inColArray = (colName: String, values: List[String]) => values.map(m => array_contains(col(colName), m)).reduce(_ || _)
+
   implicit class ACMGOperations(df: DataFrame) {
 
     /**
@@ -44,6 +52,63 @@ object ACMGImplicits {
 
         case _ => throw new IllegalArgumentException(s"Column `$colFreq` must be a StructType.")
       }
+    }
+
+    def getBS2(orphanet: DataFrame, frequencies: DataFrame): DataFrame = {
+
+      val threshold = 4
+
+      val onsets = List(
+        "Adult",
+        "Elderly",
+        "All ages",
+        "No data available")
+
+      val is_dominant_inheritance = List(
+        "Autosomal dominant",
+        "X-linked dominant",
+        "Y-linked",
+        "Mitochondrial inheritance")
+
+      val orphanetDF = orphanet.select("gene_symbol", "name", "average_age_of_onset", "type_of_inheritance")
+        .withColumn("is_adult_onset", inColArray("average_age_of_onset", onsets))
+        .na.fill(true, Seq("is_adult_onset"))
+        .filter(col("is_adult_onset") === false)
+        .withColumn("is_dominant", inColArray("type_of_inheritance", is_dominant_inheritance))
+        .select(
+          col("gene_symbol").as("symbol"),
+          col("is_dominant"))
+        .distinct()
+
+      val freqDF = frequencies
+        .select(
+          col("chromosome"),
+          col("start"),
+          col("end"),
+          col("reference"),
+          col("alternate"),
+          explode(col("genes_symbol")).as("symbol"),
+          col("external_frequencies.gnomad_genomes_3_1_1.ac").as("gnomad_ac"),
+          col("external_frequencies.gnomad_genomes_3_1_1.hom").as("gnomad_hom"))
+        .join(orphanetDF, Seq("symbol"), "leftouter")
+
+      df
+        .join(freqDF, Seq("chromosome", "start", "end", "reference", "alternate", "symbol"), "leftouter")
+        .withColumn("BS2", struct(
+          col("gnomad_ac"),
+          col("gnomad_hom"),
+          col("is_dominant"),
+          col("gnomad_hom"),
+          (
+            col("gnomad_hom").isNotNull &&
+              (
+                col("gnomad_hom") >= threshold ||
+                  (col("is_dominant") && col("gnomad_ac").isNotNull && col("gnomad_ac") >= threshold)
+                )
+            ).as("score")
+        ))
+        .drop("gnomad_ac", "gnomad_hom", "is_dominant")
+
     }
 
   }
