5861fe9fabfb75e399bd4be3098c0216bbca6913
zoemcl <zoemmclennan@gmail.com>
2024-07-04 15:03:31 -0400

    :sprakles: (feat) UNIC-705 hash transformation with column filter

--- datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/transformation/SHA256.scala
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/transformation/SHA256.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StringType
 
-case class SHA256(salt: String, override val columns: String*) extends HashTransformation {
+case class SHA256(salt: String, override val columns: String*) extends HashTransformation[Seq[String]] {
   override def transform: DataFrame => DataFrame = { df =>
     columns.foldLeft(df){ case (d, column) =>
       d.withColumn(column,
