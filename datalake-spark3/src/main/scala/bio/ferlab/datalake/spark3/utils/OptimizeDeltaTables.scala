393be9c34125cc1f35ac75547536da06a055b842
Zoé McLennan <zoemmclennan@gmail.com>
2024-09-04 11:10:23 -0400

    feat: UNIC-1349 OptimizeDeltaTables Dag (#236)
    * feat: UNIC-1349 OptimizeDeltaTables Dag
    
    * fix: UNIC-1349 formatting main args

---
+++ datalake-spark3/src/main/scala/bio/ferlab/datalake/spark3/utils/OptimizeDeltaTables.scala
@@ -0,0 +1,32 @@
+package bio.ferlab.datalake.spark3.utils
+
+import bio.ferlab.datalake.commons.config.{Configuration, RuntimeETLContext}
+import bio.ferlab.datalake.spark3.utils.DeltaUtils.{compact, vacuum}
+import mainargs.{ParserForMethods, arg, main}
+import org.apache.spark.sql.SparkSession
+
+case class OptimizeDeltaTables(rc: RuntimeETLContext, datasetIds: Seq[String], numberOfVersions: Int) {
+  implicit val conf: Configuration = rc.config
+  implicit val spark: SparkSession = rc.spark
+
+  def run(): Unit = {
+    datasetIds.foreach { id =>
+      val ds = conf.getDataset(id)
+      compact(ds)
+      vacuum(ds, numberOfVersions)
+    }
+  }
+}
+
+object OptimizeDeltaTables {
+  @main
+  def run(rc: RuntimeETLContext,
+          @arg(name = "dataset-ids", short = 'd', doc = "Dataset Ids") datasetIds: Seq[String],
+          @arg(name = "number-of-versions", short = 'n', doc = "Number of Versions") numberOfVersions: Int): Unit = {
+    OptimizeDeltaTables(rc, datasetIds, numberOfVersions).run()
+  }
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrThrow(args)
+}
+
+
